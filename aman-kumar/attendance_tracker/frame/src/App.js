1c4cadaecdbd1561089b1738c459cb890dd2dcea
amankumar739728 <124084331+amankumar739728@users.noreply.github.com>
2025-06-03 21:33:26 +0530

    Update App.js

--- aman-kumar/attendance_tracker/frame/src/App.js
+++ aman-kumar/attendance_tracker/frame/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Register from './components/Register';
